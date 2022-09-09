2ddeedca8919a1e0b06027a848eb713683981e35
= <ymecca730135@gmail.com>
2022-09-10 01:53:44 +0900

    feat : ExceptionHandlerExceptionResolver

--- src/main/java/com/example/demo/exception/ApiExceptionController.java
+++ src/main/java/com/example/demo/exception/ApiExceptionController.java
@@ -3,13 +3,32 @@ package com.example.demo.exception;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 public class ApiExceptionController {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ErrorResult illegalExHandle(IllegalArgumentException e) {
+        log.error("[exceptionHandle] ex", e);
+        return new ErrorResult("BAD", e.getMessage());
+    }
+    @ExceptionHandler
+    public ResponseEntity<ErrorResult> userExHandle(UserException e) {
+        log.error("[exceptionHandle] ex", e);
+        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
+        return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
+    }
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler
+    public ErrorResult exHandle(Exception e) {
+        log.error("[exceptionHandle] ex", e);
+        return new ErrorResult("EX", "내부 오류");
+    }
+
     @GetMapping("/api/members/{id}")
     public MemberDto getMember(@PathVariable("id") String id) {
         if (id.equals("ex")) {
@@ -18,8 +37,12 @@ public class ApiExceptionController {
         if (id.equals("bad")) {
             throw new IllegalArgumentException("잘못된 입력 값");
         }
+        if (id.equals("user-ex")) {
+            throw new UserException("사용자 오류");
+        }
         return new MemberDto(id, "hello " + id);
     }
+
     @Data
     @AllArgsConstructor
     static class MemberDto {
