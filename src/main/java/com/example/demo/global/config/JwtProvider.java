471bf5c3a85059e602c894359f73eab7003e1147
= <ymecca730135@gmail.com>
2022-09-19 21:18:03 +0900

    feat : redis Service , AuthAccount

--- src/main/java/com/example/demo/global/config/JwtProvider.java
+++ src/main/java/com/example/demo/global/config/JwtProvider.java
@@ -1,7 +1,24 @@
 package com.example.demo.global.config;
 
+import com.example.demo.domain.user.sevice.CustomAccountDetailsService;
+import com.example.demo.global.common.redis.RedisService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class JwtProvider {
+    private final CustomAccountDetailsService customAccountDetailsService;
+    private final RedisService redisService;
     public void checkRefreshToken(String email, String accessToken) {
+
+    }
+
+    public String createAccessToken(String email, String role) {
+
+    }
+
+    public String createRefreshToken(String email, String role) {
 
     }
 }
