d0e9abdba690e8613a62338517d66701048cb325
= <ymecca730135@gmail.com>
2022-09-26 09:58:29 +0900

    config : Swagger v3 fox -> docs

--- src/main/java/com/example/demo/domain/user/controller/AccountController.java
+++ src/main/java/com/example/demo/domain/user/controller/AccountController.java
@@ -5,11 +5,8 @@ import com.example.demo.domain.user.AuthUser;
 import com.example.demo.domain.user.sevice.AccountService;
 import com.example.demo.global.common.BasicResponse;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,18 +14,25 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
-@Tag(name = "Account 관련 서비스", description = "회원가입, 로그인 둥둥")
+//@Tag(name = "Account 관련 서비스", description = "회원가입, 로그인 둥둥")
+//@ApiOperation(value = "유저", notes = "ECONOVATION IDP Login Server")
 public class AccountController {
     private final AccountService accountService;
 
-//    로그아웃 기능 구현
-//    @ApiOperation(value="logout")
-    @Operation(description="로그아웃")
+    //    로그아웃 기능 구현
+    @Operation(summary = "logout", description = "로그아웃")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+    })
     @GetMapping("/logout")
-    public ResponseEntity<BasicResponse> logout(@AuthUser Account account, HttpServletRequest request){
+    public ResponseEntity<BasicResponse> logout(@AuthUser Account account, HttpServletRequest request) {
         String accessToken = request.getHeader("Authorization").substring(7);
         accountService.logout(account.getEmail(), accessToken);
         BasicResponse response = new BasicResponse("로그아웃 완료", HttpStatus.OK);
@@ -36,29 +40,26 @@ public class AccountController {
     }
 
 
-//    RefreshToken, AccessToken 재발행
+    //    RefreshToken, AccessToken 재발행
 //    @ApiOperation(value = "reIssue")
-    @Operation(description="re-issue")
+
     @GetMapping("/re-issue")
     public ResponseEntity<LoginResponseDto> reIssue(@RequestParam("email") String email, @RequestParam("refreshToken") String refreshToken) {
         LoginResponseDto responseDto = accountService.reIssueAccessToken(email, refreshToken);
         return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
 
-//  회원가입 기능 구현
-//    @ApiOperation(value = "sing-up")
-    @Operation(description="sign-up")
+    //  회원가입 기능 구현
     @PostMapping("sign-up")
-    public ResponseEntity<BasicResponse> signUp(@RequestBody SignUpRequestDto signUpUser){
-        accountService.signUp(signUpUser.getEmail(),signUpUser.getEmail(),signUpUser.getPassword());
+    public ResponseEntity<BasicResponse> signUp(@RequestBody SignUpRequestDto signUpUser) {
+        accountService.signUp(signUpUser.getEmail(), signUpUser.getEmail(), signUpUser.getPassword());
         BasicResponse response = new BasicResponse("회원가입 성공", HttpStatus.CREATED);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
+
     //     로그인 기능 구현
-//    @ApiOperation(value = "LogIn")
-    @Operation(summary = "로그인 메서드", description = "로그인 메서드입니다.")
     @PostMapping("login")
-    public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto loginDto){
+    public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto loginDto) {
         LoginResponseDto responseDto = accountService.login(loginDto.getEmail(), loginDto.getPassword());
         return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
--- src/main/java/com/example/demo/global/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/global/config/SwaggerConfig.java
@@ -1,63 +1,51 @@
 package com.example.demo.global.config;
 
-import springfox.documentation.service.Contact;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.customizers.OpenApiCustomiser;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.ArrayList;
 
 
-@Profile({"local","dev"})
-@EnableSwagger2
 @Configuration
-public class SwaggerConfig extends WebMvcConfigurationSupport {
-    private String version;
-    private String title;
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) { //spring-security와 연결할 때 이 부분을 작성하지 않으면 404에러가 뜬다.
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+public class SwaggerConfig{
+
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addRedirectViewController("/api/v1/api-docs", "/v1/api-docs");
+//        registry.addRedirectViewController("/api/swagger-resources/configuration/ui", "/swagger-resources/configuration/ui");
+//        registry.addRedirectViewController("/api/swagger-resources/configuration/security", "/swagger-resources/configuration/security");
+//        registry.addRedirectViewController("/api/swagger-resources", "/swagger-resources");
+//    }
+//
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/api/swagger-ui.html**").addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
+//        registry.addResourceHandler("/api/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+//    }
+
+    @Bean
+    public GroupedOpenApi jwtApi() {
+        return GroupedOpenApi.builder()
+                .group("jwt-api")
+                .pathsToMatch("/**")
+                .build();
     }
 
     @Bean
-    public Docket apiV1() {
-        version = "V1";
-        title = "Sol SpringBoot API " + version;
-
-        return new Docket(DocumentationType.SWAGGER_2)
-                .ignoredParameterTypes(AuthenticationPrincipal.class) // @AuthenticationPrincipal의 파라미터 요구 필드를 없애기 위함!
-                .useDefaultResponseMessages(false)
-                .groupName(version)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.springbootswagger"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo(title, version));
-    }
-
-    private ApiInfo apiInfo(String title, String version) {
-        return new ApiInfo(
-                title,
-                "Swagger로 생성한 API Docs",
-                version,
-                "www.example.com",
-                new Contact("Contact Me", "www.example.com", "foo@example.com"),
-                "Licenses",
-
-                "www.localhost:8080",
-
-                new ArrayList<>());
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info().title("Spring Boot API Example")
+                        .description("Spring Boot API 예시 프로젝트입니다.")
+                        .version("v0.0.1"));
     }
 }
